4099a3675313e2170ecb4d285ff384c838d31502
il_kimo <vibertikim@yahoo.it>
2024-05-30 17:21:11 +0200

    chore(keymap): add BASE layer progress

--- keyboards/bastardkb/charybdis/4x6/keymaps/default/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/default/keymap.c
@@ -25,21 +25,31 @@ enum charybdis_keymap_layers {
 #define LOWER MO(LAYER_LOWER)
 #define RAISE MO(LAYER_RAISE)
 
+#define LCTL_A MT(MOD_LCTL,KC_A)
+#define LSFT_R MT(MOD_LSFT,KC_R)
+#define LALT_S MT(MOD_LALT,KC_S)
+#define LGUI_T MT(MOD_LGUI,KC_T)
+#define LGUI_N MT(MOD_LGUI,KC_N)
+#define LALT_E MT(MOD_LALT,KC_E)
+#define LSFT_I MT(MOD_LSFT,KC_I)
+#define LCTL_O MT(MOD_LCTL,KC_O)
+
+
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LAYER_BASE] = LAYOUT(
   // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
-        KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,
+      QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSLS,
+      XXXXXXX,    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,       KC_J,    KC_L,    KC_U,    KC_Y,   OSL(4), XXXXXXX,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       KC_LSFT,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,       KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
+      XXXXXXX,  LCTL_A,   LSFT_R, LALT_S,  LGUI_T,    KC_G,       KC_M,  LGUI_N,  LALT_E,  LSFT_I,   LCTL_O, XXXXXXX,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_LALT,
+      XXXXXXX,    KC_Z,    LT(2,KC_X),    LT(1,KC_C),    LT(4,KC_D),    LT(6,KC_V),       KC_K,    KC_H,  MT(MOD_LCTL,KC_COMM), KC_DOT, LT(3,KC_SLSH), XXXXXXX,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
-                                   KC_LGUI, KC_SPC,   LOWER,      RAISE,  KC_ENT,
-                                           KC_LALT, KC_BSPC,     KC_DEL
-  //                            ╰───────────────────────────╯ ╰──────────────────╯
+                                   KC_SPC, KC_BSPC, XXXXXXX,      MOD_RSFT, KC_ENT,
+                                           KC_ESC, XXXXXXX,       QK_REPEAT_KEY
+ //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
 
   [LAYER_LOWER] = LAYOUT(
